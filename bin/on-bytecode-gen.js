f131aa96965199ae29c6312f15cf95d54d6ebb55
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-07-05 00:24:33 -0700

    improved decoded tree structure

--- bin/on-bytecode-gen.js
+++ bin/on-bytecode-gen.js
@@ -8,7 +8,16 @@ var lut, res;
 res = `
 'use strict';
 
+var bytecode = require('./bytecode');
+
 var i, val, res, stack;
+
+function gen (HEAP, cursor, bodySize, optr) {
+
+var HEAPU8 = new Uint8Array(HEAP);
+var HEAPU32 = new Uint32Array(HEAP);
+
+var lib = bytecode(global, null, HEAP);
 
 var codes = {`;
 
@@ -20,11 +29,15 @@ Object.keys(lut).forEach(function (key) {
     var keys = key.split('.');
     res += `
     ${lut[key]}: function (val) {
-        return {
+        var right = stack.pop();
+        var left = stack.pop();
+        stack.push({
             kind: 'binop',
             type: '${keys[0]}',
-            operator: '${keys[1]}'
-        }
+            operator: '${keys[1]}',
+            left: left,
+            right: right
+        });
     },`;
 });
 
@@ -36,11 +49,13 @@ Object.keys(lut).forEach(function (key) {
     var keys = key.split('.');
     res += `
     ${lut[key]}: function (val) {
-        return {
+        var expr = stack.pop();
+        stack.push({
             kind: 'unop',
             type: '${keys[0]}',
-            operator: '${keys[1]}'
-        }
+            operator: '${keys[1]}',
+            expr: expr
+        });
     },`;
 });
 
@@ -52,11 +67,15 @@ Object.keys(lut).forEach(function (key) {
     var keys = key.split('.');
     res += `
     ${lut[key]}: function (val) {
-        return {
+        var right = stack.pop();
+        var left = stack.pop();
+        stack.push({
             kind: 'relop',
             type: '${keys[0]}',
-            operator: '${keys[1]}'
-        }
+            operator: '${keys[1]}',
+            left: left,
+            right: right
+        });
     },`;
 });
 
@@ -68,12 +87,14 @@ Object.keys(lut).forEach(function (key) {
     var keys = key.split(/[.|\/]/);
     res += `
     ${lut[key]}: function (val) {
-        return {
+        var expr = stack.pop();
+        stack.push({
             kind: 'cvtop',
             type: '${keys[0]}',
             type1: '${keys[2]}',
-            operator: '${keys[1]}'
-        }
+            operator: '${keys[1]}',
+            expr: expr
+        });
     },`;
 });
 
@@ -85,27 +106,31 @@ Object.keys(lut).forEach(function (key) {
     var keys = key.split('.');
     res += `
     ${lut[key]}: function (val) {
-        return {
+        cursor = lib.unpack_int32(cursor, optr);
+        var imm = HEAPU32[optr >> 2];
+        stack.push({
             kind: 'const',
             type: '${keys[0]}',
-            init: '0'
-        }
+            init: imm + ''
+        });
     },`;
 });
 
 res +=`
     ${wastSpec.operators.basic.get_local}: function (val) {
-        return {
+        cursor = lib.unpack_uint32(cursor, optr);
+        var imm = HEAPU32[optr >> 2];
+        stack.push({
             kind: 'get_local',
             id: {
                 kind: 'literal',
-                value: 0,
-                raw: '0'
+                value: imm,
+                raw: imm + ''
             }
-        };
+        });
     },
     ${wastSpec.operators.basic.set_local}: function (val) {
-        return {
+        stack.push({
             kind: 'set_local',
             id: {
                 kind: 'literal',
@@ -117,7 +142,14 @@ res +=`
             //     type: 'i32',
             //     init: '0'
             // }
-        };
+        });
+    },
+    ${wastSpec.operators.basic.call_import}: function (val) {
+        var expr = stack.pop();
+        stack.push({
+            kind: 'call_import',
+            expr: expr
+        });
     },
 `;
 
@@ -137,28 +169,52 @@ lut = wastSpec.operators.memory;
 res += '\n// memory';
 
 Object.keys(lut).forEach(function (key) {
-    res += `
-    ${lut[key]}: function (val) {
-        return '${key}:' + val;
-    },`;
+    var keys = key.match(/(\w+).load(\d*)_(\w*)/);
+    if (keys) {
+        res += `
+        ${lut[key]}: function (val) {
+            cursor = lib.unpack_uint32(cursor, optr);
+            var flags = HEAPU32[optr >> 2];
+
+            cursor = lib.unpack_uint32(cursor, optr);
+            var offset = HEAPU32[optr >> 2];
+
+            var expr = stack.pop();
+            stack.push({
+                kind: 'load',
+                type: '${keys[1]}',
+                size: ${keys[2]},
+                sign: ${(keys[3] === 'u') ? false : true},
+                offset: offset,
+                align: flags,
+                expr: expr
+            });
+        },`;
+    } else {
+        res += `
+        ${lut[key]}: function (val) {
+            stack.push({
+            });
+        },`;
+    }
 });
 
 res += `
 };
 
-function gen (HEAPU8, cursor, bodySize) {
-    i = 0;
     val = 0;
     res = [];
     stack = [];
+    bodySize += cursor;
 
-    while (i < bodySize) {
-        val = HEAPU8[(cursor + i) >>0];
+    while (cursor < bodySize) {
+        val = HEAPU8[cursor >>0];
+        cursor += 1;
         if (codes[val] !== undefined) {
-            res.push(codes[val]());
+            codes[val]();
         }
-        i += 1;
     }
+    res = stack;
 
     return res;
 }
